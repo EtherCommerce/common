99bcdecf748ca4682dff4088f04e1576afbd6220
svasorcery <sva.sorcery@gmail.com>
2019-07-07 14:25:12 +0300

    [+] EC.Common.Exceptions : created domain base exception

---
+++ EtherCommerce.Common/Exceptions/EtherCommerceException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EtherCommerce.Common.Exceptions
+{
+    public class EtherCommerceException : Exception
+    {
+        public string Code { get; }
+
+
+        public EtherCommerceException()
+        {
+        }
+
+        public EtherCommerceException(string code)
+        {
+            Code = code;
+        }
+
+        public EtherCommerceException(string message, params object[] args)
+            : this(string.Empty, message, args)
+        {
+        }
+
+        public EtherCommerceException(string code, string message, params object[] args)
+            : this(null, code, message, args)
+        {
+        }
+
+        public EtherCommerceException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+        }
+
+        public EtherCommerceException(Exception innerException, string code, string message, params object[] args)
+            : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        }
+    }
+}
